ef2fb159ceca9095c6c429d9944a71d1dd95d187
hyunlee214 <hyun110214@naver.com>
2021-07-27 00:11:57 +0900

    bodyparser추가

--- log.js
+++ log.js
@@ -2,6 +2,7 @@
 
 //모듈
 const express = require ('express');
+const bodyParser = require('body-parser');
 const app = express();
 
 //const PORT = 3000;
@@ -13,6 +14,8 @@ const home = require('./src/routes/home');
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 app.use(express.static(`${__dirname}/src/public`)); 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : true}));
   
 
 app.use('/', home);        // use는 미들웨어를 등록해주는 메서드

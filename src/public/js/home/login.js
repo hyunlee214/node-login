b8d0278ec8e243a6dd6cec0ddcd10824d467d09e
hyunlee214 <hyun110214@naver.com>
2021-07-26 23:55:58 +0900

    dom사용

--- src/public/js/home/login.js
+++ src/public/js/home/login.js
@@ -1,4 +1,21 @@
 "use strict";
 
-console.log('test');
-console.log('tesging');
+//console.log('test');
+//console.log('tesging');
+
+const id = document.querySelector('#id'),   //ejs파일 안 태그
+  psword = document.querySelector('#psword'),
+  loginBtn = document.querySelector('button');
+
+loginBtn.addEventListener('click', login);
+
+function login() {
+  const req = {
+    id : id.value,
+    psword: psword.value,
+  };
+  console.log(req);
+  
+}
+
+ 

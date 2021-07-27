5571916a5a86ed284cd4ead7353dfaeb1866e1c0
hyunlee214 <hyun110214@naver.com>
2021-07-27 23:22:43 +0900

    사용자저장소 재구현

--- src/models/UserStorage.js
+++ src/models/UserStorage.js
@@ -1,7 +1,7 @@
 "use strict";
 
 class UserStorage {
-  static #users = {
+  static #users = {      //변수 은닉화 
     id: ['hyunlee', 'jo', 'han'],
     psword: ['0000', '1111', '2222'],
  
@@ -11,10 +11,10 @@ class UserStorage {
   static getUsers(...fields) {
     const users = this.#users;
     const newUsers = fields.reduce((newUsers, field) => {
-      if (users.hasOwnProperty(field)) {
+      if (users.hasOwnProperty(field)) {    //users에 해당하는 key값이 있는지
         newUsers[field] = users[field];
       }
-      return newUsers;
+      return newUsers;    //return되는 newUsers가 다음 파라미터로 들어가면서 계속해서 반복 
         }, {});
     return newUsers;
   }
--- src/routes/home/home.ctrl.js
+++ src/routes/home/home.ctrl.js
@@ -11,38 +11,26 @@ const output = {
     res.render('home/login');
   },
 };
- 
- 
-const users = { 
-  id: ['hyunlee', 'jo', 'han'],
-  psword: ['0000', '1111', '2222'],
-}; 
 
 const process = {
   login: (req, res) => {
     const id = req.body.id,
     psword = req.body.psword;
  
-    const users = UserStorage.getUsers('id','psword');  
+    const users = UserStorage.getUsers('id','psword');   //로그인 검증 기능
 
-    const response = {};
+    const response = {};    //오브젝트 생성
     if (users.id.includes(id)) {
       const idx = users.id.indexOf(id);
       if (users.psword[idx] === psword) {
         response.success = true;
-        return res.json({
-          success : true,
-        });
+        return res.json(response);
       }
     }
+    response.success = false;
+    response.msg = 'login실패';
+    return res.json(response);
 
-     return res.json({
-       success: false,
-       msg : 'login 실패',
-     })
-     // response.success = false;
-    // response.msg = "login실패";
-    // return res.json(response);
   },
 };
 

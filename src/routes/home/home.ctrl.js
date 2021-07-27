da6e60a32e458cec76a957769f0c80cc5152f77c
hyunlee214 <hyun110214@naver.com>
2021-07-28 00:58:18 +0900

    회원가입페이지 추가

--- src/routes/home/home.ctrl.js
+++ src/routes/home/home.ctrl.js
@@ -7,10 +7,13 @@ const output = {
   home: (req, res) => {
     res.render('home/index');
   },
-  
+   
   login: (req, res) => {
     res.render('home/login');
   },
+  register: (req, res) => {
+    res.render('home/register'); 
+  }
 };
 
 // 기존 로직 없이 'users.login'으로만 login기능을 가능하게 구현

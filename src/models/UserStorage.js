cc54d043341f629ed861fc32d57f79c1724e558b
hyunlee214 <hyun110214@naver.com>
2021-08-01 00:32:20 +0900

    id데이터 정보추가기능

--- src/models/UserStorage.js
+++ src/models/UserStorage.js
@@ -4,7 +4,7 @@ const e = require('express');
 const fs = require('fs').promises;    // fs를 이용해 파일에 접근
 
 class UserStorage {
-  static #getUserInfo(data, id) {       //private한 변수나 메서드는 항상 클래스의 최상단에 위치하는게 좋음. 
+  static #getUserInfo(data, fields) {       //private한 변수나 메서드는 항상 클래스의 최상단에 위치하는게 좋음. 
     const users = JSON.parse(data);
     const idx = users.id.indexOf(id);
     const usersKeys = Object.keys(users);        
@@ -16,8 +16,10 @@ class UserStorage {
     return userInfo;
   }
 
-  static getUsers(...fields) {
-    // const users = this.#users;
+  static #getUsers(data, isAll, fields) {
+    const users = JSON.parse(data);
+    if (isAll) return users;
+
     const newUsers = fields.reduce((newUsers, field) => {
       if (users.hasOwnProperty(field)) {    //users에 해당하는 key값이 있는지
         newUsers[field] = users[field];
@@ -26,21 +28,36 @@ class UserStorage {
         }, {});
     return newUsers;
   }
+  
+  static getUsers(isAll, ...fields) {
+    return fs
+    .readFile('./src/databases/users.json')
+    .then(data => {
+      return this.#getUsers(data, isAll, fields);
+    })
+    .catch(console.error); 
+  }
+
   static getUsersInfo(id) {
-    return fs.readFile('./src/databases/users.json')
+    return fs
+    .readFile('./src/databases/users.json')
     .then(data => {
       return this.#getUserInfo(data, id);
     })
     .catch(console.error); 
   }
 
-  static save(userInfo) {                 // 클라이언트에서 전달한 데이터를 저장해주는 함수 생성
-    // const users = this.#users;
+  static async save(userInfo) {                 // 클라이언트에서 전달한 데이터를 저장해주는 함수 생성
+    const users = await this.getUsers(true);
+    if (users.id.includes(userInfo.id)) {
+      return new Error('이미 존재하는 아이디입니다.');
+    } 
     users.id.push(userInfo.id);
     users.name.push(userInfo.name);
     users.psword.push(userInfo.psword);
-    return { success : true }; 
+    fs.writeFile('./src/databases/users.json', JSON.stringify(users));
+    return { success: true };
   }
-} 
+}   
 
 module.exports = UserStorage;
--- src/public/js/home/register.js
+++ src/public/js/home/register.js
@@ -12,11 +12,10 @@ registerBtn.addEventListener('click', register);
 
 function register() {
   if (!id.value) return alert ('아이디를 꼭 입력해주세요'); 
-
-  if (psword.value !== confirmPsword.value) {
-    return alert('비밀번호가 서로 다릅니다');
-  }
-
+ 
+  if (psword.value !== confirmPsword.value) 
+    return alert ('비밀번호가 서로 다릅니다');
+ 
   const req = {
     id : id.value,
     name : name.value,

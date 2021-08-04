3d948a5623c5a96d78e5ca30f88c6e88c972db61
hyunlee214 <hyun110214@naver.com>
2021-08-04 23:09:44 +0900

    회원가입기능추가, 환경변수적용전

--- src/models/UserStorage.js
+++ src/models/UserStorage.js
@@ -5,39 +5,13 @@ const e = require('express');
 /*const fs = require("fs").promises;    // fs를 이용해 파일에 접근 */
 
 class UserStorage {
-  static #getUserInfo(data, id) {       //private한 변수나 메서드는 항상 클래스의 최상단에 위치하는게 좋음. 
-    const users = JSON.parse(data);
-    const idx = users.id.indexOf(id);
-    const usersKeys = Object.keys(users);        
-    const userInfo = usersKeys.reduce((newUser, info) => {
-      newUser[info] = users[info][idx];
-      return newUser;
-    }, {});
-    
-    return userInfo;
-  }
 
-  static #getUsers(data, isAll, fields) {
-    const users = JSON.parse(data);
-    if (isAll) return users;
 
-    const newUsers = fields.reduce((newUsers, field) => {
-      if (users.hasOwnProperty(field)) {    //users에 해당하는 key값이 있는지
-        newUsers[field] = users[field];
-      }
-      return newUsers;    //return되는 newUsers가 다음 파라미터로 들어가면서 계속해서 반복 
-        }, {});
-    return newUsers;
-  }
-  
-  static getUsers(isAll, ...fields) {
-
-  }
-
-  static getUserInfo(id) {
-    return new Promise((resolve, reject) => {         // Promise안에 있는 구문이 성공하면 resolve값, 실패하면 reject값 실행
-    db.query("SELECT * FROM users WHERE id = ?",[id], (err, data) => {
-        if (err) reject(err);
+  static getUserInfo(id) {                            // Promise안에 있는 구문이 성공하면 resolve값, 실패하면 reject값 실행
+    return new Promise((resolve, reject) => {          
+      const query = "SELECT * FROM users WHERE id = ?;";
+      db.query(query,[id], (err, data) => {
+        if (err) reject(`${err}`);
         console.log(data[0]);
         resolve(data[0]);
       });  
@@ -45,6 +19,13 @@ class UserStorage {
   }
 
   static async save(userInfo) {
+    return new Promise((resolve, reject) => {          
+      const query = "INSERT INTO users(id, name, psword) VALUES(?, ?, ?);";
+      db.query(query,[userInfo.id, userInfo.name, userInfo.psword], (err) => {
+        if (err) reject(`${err}`);
+        resolve ({ success: true });
+      });  
+  });
 
   }
 }

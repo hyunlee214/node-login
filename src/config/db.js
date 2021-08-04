74b0f8cae0bc1c1519c85fac6460439cdfa584b7
hyunlee214 <hyun110214@naver.com>
2021-08-04 22:16:24 +0900

    db.js파일 오타발견

--- src/config/db.js
+++ src/config/db.js
@@ -3,8 +3,8 @@ const mysql = require("mysql");
 const db = mysql.createConnection({
   host: "node-login.cxwohdc7ogaq.ap-northeast-2.rds.amazonaws.com",
   user: "admin",
-  pssword: "ajddhkf1234",
-  database: "node-login",
+  password: "ajddhkf1234",
+  database: "node_login",
 });
 
 db.connect(); 
